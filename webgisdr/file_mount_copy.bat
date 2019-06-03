8b2c3ed3b6cabc2df1283a41f2eb4ec33818f887
WML2013 <mingles@gmail.com>
2019-06-03 15:25:07 +0100

    API UPDATE

--- webgisdr/file_mount_copy.bat
+++ webgisdr/file_mount_copy.bat
@@ -9,7 +9,7 @@ REM First remove the mapping
 net use z:/delete
 
 REM now mount the Azure file as the 'z' drive.
-net use Z: \\sdmsstorage1.file.core.windows.net\webgisdrbackups /u:AZURE\sdmsstorage1 nuuda7FHdpjDH3zgLeT41Wu5MPlQwvk7m5maISYxiOI4yDmWqAqeM9JwPSS6IFpwvdm+zuCxj2vF8OgxjbTeXw==
+net use Z: \\sdmsstorage1.file.core.windows.net\webgisdrbackups /u:AZURE\sdmsstorage1 [API KEY]
 
 REM Use robocopy to copy the files and folders to the Azure file location.
 robocopy c:\webgisdr_backups Z:\dev1_webgisdr_backups /COPYALL
